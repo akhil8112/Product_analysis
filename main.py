f9fbfb102d68b3a0c49f154bebad48ca13b38eb2
akhil8112 <148536710+akhil8112@users.noreply.github.com>
2025-04-07 23:56:26 +0530

    main.py

--- main.py
+++ main.py
@@ -2,4 +2,18 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-file_path=read._excel("")
+#Load Dataset
+file_path="Recommendation_excel.xlsx"
+data=pd.read_excel(file_path, sheet_name='recommendation')
+print("Load Successfully")
+
+#Cleaning of the data
+#Data Information
+# Gives the information about the data
+data.info()
+
+#Perform the statistical function on the data
+print(data.describe())
+
+#Is any null values are present or not if it is present then print how many null values are there 
+print("Total null values: \n",data.isnull().sum())
